ad68ee76b4a3114feb5deae6bf5dc7ff842a36c3
Jin Ha <jsh114@tui.doc.ic.ac.uk>
2018-03-01 21:18:07 +0000

    Refactored overfit_fcnet so that we are achieving full accuracy within 20 epochs

--- neural-network/src/overfit_fcnet.py
+++ neural-network/src/overfit_fcnet.py
@@ -10,17 +10,21 @@ TODO: Overfit the network with 50 samples of CIFAR-10
 ###########################################################################
 #                           BEGIN OF YOUR CODE                            #
 ###########################################################################
-model = FullyConnectedNet([20, 30])
+# define model and data
+model = FullyConnectedNet(hidden_dims=[20, 30])
 data = get_CIFAR10_data(num_training=50)
+
+# define solver which helps us to train our model using the data
 solver = Solver(
     model,
     data,
-    lr_decay=0.9,
-    batch_size=10,
     num_epochs=20,
     num_train_samples=50
 )
+
+# train our model using the solver
 solver.train()
 ##############################################################################
 #                             END OF YOUR CODE                               #
 ##############################################################################
+
